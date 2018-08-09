2ad5e60325a76d26337c360e09c7473ebe933beb
cb <csbutton01@hotmail.com>
2018-08-08 19:44:03 -0700

    implement scss chokidar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import { HashRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import TextArea from './components/TextArea';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <TextArea text="Cameron Button" />
+        <div className="tagLine">
+            <TextArea text="Indie Game Dev"/>
+            <TextArea text="Web Dev" />
+          </div>
         </header>
-        <TextArea text="This is a test for the text area"/>
       </div>
     );
   }
